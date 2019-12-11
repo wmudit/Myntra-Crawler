2f9de35743e9bc188bdf687d9a6961e6cdfd4b39
Mudit <wmudit@gmail.com>
2019-12-11 22:49:31 +0530

    v1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let helper = require('./helpers/helper')
 let utils = require('./lib/utils')
 let constants = require('./lib/constants')
+var fs = require('fs');
 
 async function crawl () {
     try {
@@ -9,11 +10,21 @@ async function crawl () {
         console.log(xid);
         let products = [];
         for(let category of categories) {
-            products = products.concat([...await helper.getProductsInCategory(browser, category, xid)]);
+            products = await helper.getProductsInCategory(browser, category, xid);
+            // products = products.concat([...await helper.getProductsInCategory(browser, category, xid)]);
+            if(products)
+                writeToFile(products);
             await utils.sleep(3 * 1000);
         }
+        await utils.sleep(60 * 1000);
     } catch (err) {
         console.log(err.stack);
+    }
+}
+
+function writeToFile (products) {
+    for(let i = 0; i < products.length; i++) {
+        fs.appendFileSync('products.json', JSON.stringify(products[i]) + ",\n");
     }
 }
 
